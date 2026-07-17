375daf0370ea50cbeacfdea61696324a2ae9d36c
Nazmus Sakib <150610050+Sakib2405@users.noreply.github.com>
2026-07-18 01:00:39 +0600

    Create iterative_dfs.py

---
+++ 5th Semister/CIt-315(AI)/Mid Master Lab/AI Sessional Mid/Puspita/Masud sir/iterative_dfs.py
@@ -0,0 +1,28 @@
+tree = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['G', 'H'],
+    'F': [],
+    'G': [],
+    'H': ['P'],
+    'P': [] 
+}
+from collections import deque
+def dfs_limited(tree, start, limit, visited=[]):
+    if limit <= 0:
+        return
+    if start not in visited:
+        print(start, end=" ")
+        visited.append(start)
+    for node in tree[start]:
+        dfs_limited(tree, node, limit-1, visited)
+
+def iterative_deepening(tree, start, max_limit):
+    for i in range(max_limit):
+        print(f"Iteration {i+1} : ", end="")
+        dfs_limited(tree, start, i+1, [])
+        print()
+
+iterative_deepening(tree, 'A', 4)
